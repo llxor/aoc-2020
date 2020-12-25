b426c218fce3e9c27391d757698b190df15a46b5
llxor <llxoyd@gmail.com>
2020-12-25 22:34:13 +0000

    Optimise Day 1 Part 1 to O(N) and add benchmarks

---
+++ BENCHMARKS.md
@@ -0,0 +1,6 @@
+**Benchmarks:**
+The following programs where compiled using the zig compiler using -O3 on an Intel i3 Processor. Timings were recorded using the Linux `time` command and are in milliseconds.
+
+| Day | Part 1 | Part 2 |
+|----:|-------:|-------:|
+|   1 |    002 |        |
--- src/01A.c
+++ src/01A.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-int main() {
-  const int N = 200;
-  int array[N] = {};
+int main()
+{
+    int val, array[2020] = {};
 
-  for (int i = 0; i < N; i++) {
-    scanf("%d", &array[i]);
-  }
-
-  for (int i = 0; i < N; i++) {
-    for (int j = i + 1; j < N; j++) {
-      if (array[i] + array[j] == 2020) {
-        printf("%d\n", array[i] * array[j]);
-        return 0;
-      }
+    while (1) {
+        scanf("%d", &val);
+        if (array[val]) break;
+        array[2020 - val] = 1;
     }
-  }
+
+    printf("%d\n", val * (2020 - val));
 }
