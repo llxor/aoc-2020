bd48a64de702bac7a7eadd1f6b6f8f1a6c749f97
llxor <llxoyd@gmail.com>
2021-01-05 20:37:25 +0000

    Move history to global array

--- src/15.c
+++ src/15.c
@@ -4,13 +4,13 @@
 // change to 2020 for part 1
 const int size = 30000000;
 char set[size >> 3] = {};
+int hist[size] = {};
 
 int contains(int x) { return (set[x >> 3] >> (x & 7)) & 1; }
 void insert(int x) { set[x >> 3] |= 1 << (x & 7); }
 
 int main() {
   int input[6] = {1, 12, 0, 20, 8, 16};
-  int *hist = calloc(size, sizeof(int));
 
   for (int i = 0; i < 6; i++) {
     hist[input[i]] = i + 1;
